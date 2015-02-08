869a9cdd449483524920fb0d9cd8447833cd74a2
Sujith Vishwajith <sujithv28@gmail.com>
2015-02-08 12:10:36 -0800

    added product detail view

--- Bundler/Bundler/NewBundleViewController.swift
+++ Bundler/Bundler/NewBundleViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class NewBundleViewController: UIViewController, UITextFieldDelegate {
+class ProductListCell : UITableViewCell {
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var detailCost: UILabel!
+    
+    func loadDetail(#title: String, itemCost: Int, image: String) {
+        detailImage.image = UIImage(named: image)
+        detailTitle.text = title
+        detailCost.text = "$" + String(itemCost)
+    }
+    
+}
+
+class NewBundleViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var bundleTitleField: UITextField!
     @IBOutlet weak var bundleItemField: UITextField!
     @IBOutlet weak var tableViewList: UITableView!
+    
+    var items: [(String, Int, String)] = [
+        ("Magic Candles", 5, "party.png"),
+        ("Magic Candles", 5, "party.png")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +41,42 @@ class NewBundleViewController: UIViewController, UITextFieldDelegate {
         self.navigationItem.setHidesBackButton(true, animated: false)
         self.bundleItemField.delegate = self;
         self.bundleTitleField.delegate = self;
+        
+        var nib = UINib(nibName: "ProductTableViewCell", bundle: nil)
+        tableViewList.registerNib(nib, forCellReuseIdentifier: "customCell")
     }
     
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:ProductListCell = self.tableViewList.dequeueReusableCellWithIdentifier("customCell") as ProductListCell
+        
+        // this is how you extract values from a tuple
+        var (title, itemCost, image) = items[indexPath.row]
+        
+        cell.loadDetail(title: title, itemCost: itemCost, image: image)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 45;
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 45;
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        println("You selected cell #\(indexPath.row)!")
+        self.performSegueWithIdentifier("toDetailView", sender: self)
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count;
+    }
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true);
     }
